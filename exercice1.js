b5d646e94d9f54da0a705a98c2a36de36ec6d2a4
Baptiste <baptiste@baptiste-IMEDIA-8804.(none)>
2013-10-26 16:00:38 +0200

    exercie 1 javascript

---
+++ exercice1.js
@@ -0,0 +1,48 @@
+
+// old adder, with one argument
+function oldAdder(l, r){
+  return function(v){
+    return l(v) + r(v);
+  }
+}
+
+// mult
+function mult(v){
+  return function(e){
+    return v*e;
+  }
+}
+
+// sub
+function sub(v){
+  return function(e){
+    return v-e;
+  }
+}
+
+// adder with with a variable number of arguments.
+function adder() {
+	var arg = Array.prototype.slice.call(arguments); // On transforme l'objet en Array
+
+	return function(v){
+		var res = 0;
+		arg.forEach(function(value) {res += value(v);});
+		return res;
+	}
+}
+
+
+
+
+// Test:
+console.log(adder(mult(2), mult(4))(2));			// 12
+console.log(sub(0)(0)); 							//  0
+console.log(sub(2)(1)); 							//  1
+console.log(sub(2)(2)); 							//  0
+console.log(sub(2)(4)); 							// -2
+console.log(adder()(0));							//  0
+console.log(adder()(1)); 							//  0
+console.log(adder(mult(2))(1)); 					//  2
+console.log(adder(mult(2), mult(2))(1)); 			//  4
+console.log(adder(mult(2), mult(2), mult(2))(1)); 	//  6
+console.log(adder(mult(2), sub(2), mult(2))(1)); 	//  5
