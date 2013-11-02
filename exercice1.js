867612b35cb68e47f3fe347430bc226e4ea06207
Baptiste-Pignol <baptiste.pignol@laposte.net>
2013-11-02 15:04:32 +0100

    Update exercice1.js
    Final version of the exercise 1.

--- exercice1.js
+++ exercice1.js
@@ -1,47 +1,49 @@
+'use strict';
+
 // old adder, with one argument
-function oldAdder(l, r){
-  return function(v){
-    return l(v) + r(v);
-  }
+function oldAdder(l, r) {
+    return function (v) {
+        return l(v) + r(v);
+    };
 }
 
 // mult
-function mult(v){
-  return function(e){
-    return v*e;
-  }
+function mult(v) {
+    return function (e) {
+        return v * e;
+    };
 }
 
 // sub
-function sub(v){
-  return function(e){
-    return v-e;
-  }
+function sub(v) {
+    return function (e) {
+        return v - e;
+    };
 }
 
 // adder with with a variable number of arguments.
-function adder(/*arguments...*/) {
-	var arg = Array.prototype.slice.call(arguments); // transform the object in Array
+function adder() {
+    var arg = Array.prototype.slice.call(arguments); // transform the object in Array
 
-	return function(v){
-		var res = 0;
-		arg.forEach(function(value) {res += value(v);});
-		return res;
-	}
+    return function (v) {
+        var res = 0;
+        arg.forEach(function (value) {res += value(v); });
+        return res;
+    };
 }
 
 
 
 
 // test:
-console.log(adder(mult(2), mult(4))(2));		// 12
-console.log(sub(0)(0)); 				//  0
-console.log(sub(2)(1)); 				//  1
-console.log(sub(2)(2)); 				//  0
-console.log(sub(2)(4)); 				// -2
-console.log(adder()(0));				//  0
-console.log(adder()(1)); 				//  0
-console.log(adder(mult(2))(1)); 			//  2
-console.log(adder(mult(2), mult(2))(1)); 		//  4
-console.log(adder(mult(2), mult(2), mult(2))(1)); 	//  6
-console.log(adder(mult(2), sub(2), mult(2))(1)); 	//  5
+console.log(adder(mult(2), mult(4))(2));                 // 12
+console.log(sub(0)(0));                                  //  0
+console.log(sub(2)(1));                                  //  1
+console.log(sub(2)(2));                                  //  0
+console.log(sub(2)(4));                                  // -2
+console.log(adder()(0));                                 //  0
+console.log(adder()(1));                                 //  0
+console.log(adder(mult(2))(1));                          //  2
+console.log(adder(mult(2), mult(2))(1));                 //  4
+console.log(adder(mult(2), mult(2), mult(2))(1));        //  6
+console.log(adder(mult(2), sub(2), mult(2))(1));         //  5
